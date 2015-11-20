0c4ef760bd224bbc28e0b0121297131cdb80207c
Sean Murphy <seanduganmurphy@gmail.com>
2015-11-20 12:45:11 -0700

    First Commit
    Firebase Enabled
    basic signup error handling

---
+++ Mess/FirebaseController.swift
@@ -0,0 +1,9 @@
+//
+//  FirebaseController.swift
+//  Mess
+//
+//  Created by Sean Murphy on 11/16/15.
+//  Copyright © 2015 Sean Murphy. All rights reserved.
+//
+
+import Foundation
---
+++ Mess/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Mess
+//
+//  Created by Sean Murphy on 11/16/15.
+//  Copyright © 2015 Sean Murphy. All rights reserved.
+//
+
+import Foundation
